83ebf17bb674f91e24697f8f7251cb5688b645b6
박대진 <qkreo321@gmail.com>
2026-05-20 09:55:30 +0900

    [Passed] 합과 평균의 차 (62ms, 10MB)

---
+++ trail0/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ');
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+let sum = a+b+c;
+let avg = (a+b+c)/3;
+let n = sum -avg;
+
+console.log(`${sum}\n${avg}\n${n}`);
